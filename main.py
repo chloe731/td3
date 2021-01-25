03e28bca48c29e4785918f18fff4393df7c4c43c
stidsem4 <colin@cgoutte.fr>
2021-01-25 22:38:33 +0000

    maj apres verifs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from bibliotheque import fonction2
+
+assert fonction2() == 2
+#assert fonction2() == 3, f"fonction2() ne vaut pas 3"
+
 from td3.bibliotheque import mafonction
 
 print(mafonction())
 
 assert mafonction() == 3
 
-from lib import fonction2
-
-assert fonction2() == 2
-#assert fonction2() == 3, f"fonction2() ne vaut pas 3"
